314ec81209a0196b2522de9665ccc541ce8d95c7
MichaelDominic <michaelchristiandominic@gmail.com>
2024-03-08 22:59:48 +0700

    535230221 michael christian dominic

---
+++ Latihan_1.js
@@ -0,0 +1,19 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+  
+  function displayPrimesInRange(start, end) {
+    for (let i = start; i <= end; i++) {
+      if (isPrime(i)) {
+        console.log(i);
+      }
+    }
+  }
+  
+  // Menampilkan bilangan prima dari 1 hingga 1000
+  displayPrimesInRange(1, 1000);
+  
